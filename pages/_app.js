b9879f5cc8fa003aa2cacb75cccdef0a56beec2e
postmann84 <outoforder84@gmx.de>
2025-02-20 15:42:48 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Force a refresh of styles
+    if (typeof window !== 'undefined') {
+      const style = document.createElement('style')
+      style.textContent = ''
+      document.head.appendChild(style)
+    }
+  }, [])
+
   return (
     <div className="min-h-screen bg-gray-50">
       <Component {...pageProps} />
-      <style jsx global>{`
-        @import 'tailwindcss/base';
-        @import 'tailwindcss/components';
-        @import 'tailwindcss/utilities';
-      `}</style>
     </div>
   )
 }
