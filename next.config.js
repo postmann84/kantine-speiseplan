2fb26f854d86c3fbb22fd5843be45fbe04682aae
postmann84 <outoforder84@gmx.de>
2025-02-20 15:45:08 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader']
-    });
-    return config;
-  },
-  experimental: {
-    appDir: false,
-  }
 }
 
 module.exports = nextConfig
