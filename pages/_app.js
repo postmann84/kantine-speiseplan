7a1231c0d87f56401ce6bf246924b67454878568
postmann84 <outoforder84@gmx.de>
2025-02-20 14:57:29 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Component {...pageProps} />
+      <style jsx global>{`
+        @import 'tailwindcss/base';
+        @import 'tailwindcss/components';
+        @import 'tailwindcss/utilities';
+      `}</style>
+    </div>
+  )
 }
 
 export default MyApp
