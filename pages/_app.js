0d6b29e97f5a6bd3cf1707bd073a2eae8cf2117d
postmann84 <outoforder84@gmx.de>
2025-02-20 15:45:44 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,7 @@
 import '../styles/globals.css'
-import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Force a refresh of styles
-    if (typeof window !== 'undefined') {
-      const style = document.createElement('style')
-      style.textContent = ''
-      document.head.appendChild(style)
-    }
-  }, [])
-
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
