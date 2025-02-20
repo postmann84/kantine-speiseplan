da5f3a02beb69a94922a36acc8132d0eb66f3253
postmann84 <outoforder84@gmx.de>
2025-02-20 11:07:12 +0100

    Update menu.js

--- models/menu.js
+++ models/menu.js
@@ -1,1 +1,30 @@
+// models/menu.js
+import mongoose from 'mongoose';
 
+const MenuSchema = new mongoose.Schema({
+  weekStart: {
+    type: Date,
+    required: true
+  },
+  weekEnd: {
+    type: Date,
+    required: true
+  },
+  days: [{
+    day: String,
+    meals: [{
+      name: String,
+      price: Number
+    }]
+  }],
+  contactInfo: {
+    phone: String,
+    postcode: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.models.Menu || mongoose.model('Menu', MenuSchema);
